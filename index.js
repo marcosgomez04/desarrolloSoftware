b0fc9bde9fd9793c825fb2904cfa04741bb45a36
marcosgomez04 <mgbdndgg@gmail.com>
2025-05-09 17:43:55 -0300

    advertencia

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ console.log ("hola marquitos");
 console.log("No me modifiquen nada porfa, soy nuevo");
 >>>>>>> 1dc59968bcfb1106d9c07fdd33363d029de32e08
 console.log("no me borres oka");
+
+console.log("Me vuelven a modificar el archivo y les desaparece un familiar");
